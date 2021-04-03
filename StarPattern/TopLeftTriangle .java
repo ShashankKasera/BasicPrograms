ebeaaa264ebd9356985f58a597db352a0e2c48c0
ShashankKasera <shashankkasera98@gmail.com>
2021-04-03 14:56:38 +0530

    TopLeftTriangle

--- StarPattern/TopLeftTriangle .java
+++ StarPattern/TopLeftTriangle .java
@@ -10,11 +10,11 @@ public class TopLeftTriangle
 		n=s.nextInt();
 		
 		for(int i=1;i<=n;i++){
-           for(int j=1;j<=i;j++){
-              System.out.print("* ");
-           }
-           System.out.println("");
-        }
+                    for(int j=1;j<=i;j++){
+                       System.out.print("* ");
+                    }
+                    System.out.println("");
+                }
 	}
 }
 
