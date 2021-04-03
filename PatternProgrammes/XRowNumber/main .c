bfd6c419bd07256bdeba464fc7bd0c23cbb87718
ShashankKasera <shashankkasera98@gmail.com>
2021-04-03 22:22:50 +0530

    X Row Number programme

---
+++ PatternProgrammes/XRowNumber/main .c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+void main(){
+    
+    int n;
+    printf("Enter a number ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==j || (n-i+1)==j)
+              printf("%d ",i);
+            else 
+              printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+/*
+============ OUTPUT ============
+	
+	Enter a number 9
+        1               1                                                                                                             
+	  2           2                                                                                                               
+	    3       3                                                                                                                 
+	      4   4                                                                                                                   
+	        5                                                                                                                     
+	      6   6                                                                                                                   
+	    7       7                                                                                                                 
+	  8           8                                                                                                               
+	9               9
+*/
