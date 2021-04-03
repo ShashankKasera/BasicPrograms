8ba258b820609d5497121399a3ccf5190f1a7b2b
ShashankKasera <shashankkasera98@gmail.com>
2021-04-03 15:37:46 +0530

    TopRightTriangle

---
+++ StarPattern/BottomLeftTriangle.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner; 
+public class BottomLeftTriangle
+{
+	public static void main(String[] args) {
+		
+		int n=0;;
+		System.out.print("Enter a number ");
+		Scanner s = new Scanner(System.in);
+		n=s.nextInt();
+		
+		for(int i=1;i<=n;i++){
+                   for(int j=1;j<=n;j++){
+                      if((n-i)+1>=j)
+                        System.out.print("* ");
+                      else 
+                        System.out.print("  ");
+                   }
+                   System.out.println("");
+                }
+	}
+}
+
+/*
+Enter a number 10                                                                                                             
+* * * * * * * * * *                                                                                                             
+* * * * * * * * *                                                                                                               
+* * * * * * * *                                                                                                                 
+* * * * * * *                                                                                                                   
+* * * * * *                                                                                                                     
+* * * * *                                                                                                                       
+* * * *                                                                                                                         
+* * *                                                                                                                           
+* *                                                                                                                             
+*
+
+*/
--- StarPattern/TopRightTriangle.java
+++ StarPattern/TopRightTriangle.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner; 
-public class Main
+public class TopRightTriangle
 {
 	public static void main(String[] args) {
 		
