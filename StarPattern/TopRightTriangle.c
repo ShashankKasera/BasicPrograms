14e04e1cc426c7479a12708ae987fbccd7a6c16f
ShashankKasera <shashankkasera98@gmail.com>
2021-04-03 15:17:18 +0530

    TopRightTriangle

---
+++ StarPattern/TopRightTriangle.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+void main(){
+    
+    int n;
+    printf("Enter a number ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(n-i<j)
+              printf("* ");
+            else 
+              printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+/*
+Enter a number 10                                                                                                             
+*                 *                                                                                           
+                * *                                                                                                             
+              * * *                                                                                                             
+            * * * *                                                                                                             
+          * * * * *                                                                                                             
+        * * * * * *                                                                                                             
+      * * * * * * *                                                                                                             
+    * * * * * * * *                                                                                                             
+  * * * * * * * * *                                                                                                             
+* * * * * * * * * *
+
+*/
+
+
