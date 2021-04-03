0bd8f6306dfc1a067426ff19f9838fd5d2f28aec
ShashankKasera <shashankkasera98@gmail.com>
2021-04-03 17:02:11 +0530

    Top Left Triangle Row Number programme

---
+++ PatternProgrammes/TopLeftTriangleRowNumber/Main.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner; 
+public class Main 
+{
+	public static void main(String[] args) {
+		
+		int n=0;;
+		System.out.print("Enter a number ");
+		Scanner s = new Scanner(System.in);
+		n=s.nextInt();
+		
+		for(int i=1;i<=n;i++){
+                    for(int j=1;j<=i;j++){
+                       System.out.print(i+" ");
+                    }
+                    System.out.println("");
+                }
+	}
+}
+
+/*
+============ OUTPUT ============
+
+	Enter a number 9                                                                                                             
+	1                                                                                                                               
+	2 2                                                                                                                             
+	3 3 3                                                                                                                           
+	4 4 4 4                                                                                                                         
+	5 5 5 5 5                                                                                                                       
+	6 6 6 6 6 6                                                                                                                     
+	7 7 7 7 7 7 7                                                                                                                   
+	8 8 8 8 8 8 8 8                                                                                                                 
+	9 9 9 9 9 9 9 9 9 
+
+*/
