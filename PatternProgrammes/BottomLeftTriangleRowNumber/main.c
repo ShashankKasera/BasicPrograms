cc32974684ef29766feca2959b2b1aed7f85f5ad
ShashankKasera <shashankkasera98@gmail.com>
2021-04-03 17:46:55 +0530

    Bottom Left Triangle Row Number programme

---
+++ PatternProgrammes/BottomLeftTriangleRowNumber/main.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+void main(){
+    
+    int n;
+    printf("Enter a number ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if((n-i)+1>=j)
+              printf("%d ",i);
+            else 
+              printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+/*
+============ OUTPUT ============
+	
+	Enter a number 9                                                                                                             
+	1 1 1 1 1 1 1 1 1                                                                                                             
+	2 2 2 2 2 2 2 2                                                                                                               
+	3 3 3 3 3 3 3                                                                                                                 
+	4 4 4 4 4 4                                                                                                                   
+	5 5 5 5 5                                                                                                                     
+	6 6 6 6                                                                                                                       
+	7 7 7                                                                                                                         
+	8 8                                                                                                                           
+	9
+*/
+
+
+
