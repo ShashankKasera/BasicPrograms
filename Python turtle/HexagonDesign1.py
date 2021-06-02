e5af48c408b0e96326993460003b201b1740bc01
ShashankKasera <shashankKasera98@gmail.com>
2021-06-02 23:21:31 +0530

    HexagonDesign1

---
+++ Python turtle/HexagonDesign1.py
@@ -0,0 +1,17 @@
+
+import turtle
+
+t=turtle.Turtle()
+sc=turtle.Screen()
+sc.setup(700,700)
+sc.bgcolor('black')
+col=('pink','aqua','indigo','snow')
+t.speed(0)
+    
+for b in range(150):
+        t.pencolor(col[b%4])
+        t.width(5)
+        t.forward(b*2)
+        t.right(300)
+
+turtle.exitonclick() 
